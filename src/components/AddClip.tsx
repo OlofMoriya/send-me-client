5bcab4c24dd479107f5eee60f6229176ba71abb9
OlofMoriya <olof.moriya@gmail.com>
2023-01-29 10:16:34 +0100

    "add field for reciever"

--- src/components/AddClip.tsx
+++ src/components/AddClip.tsx
@@ -4,13 +4,14 @@ import { postClip, useClips } from "~/stores/clips_store";
 export default function AddClipForm() {
     const [_, refetchClips] = useClips() as [any, any];
     const [text, setText] = createSignal("");
+    const [reciever, setReciever] = createSignal<string | undefined>(undefined);
     const [sending, setSending] = createSignal(false);
     const [error, setError] = createSignal(false);
     const sendAction = async () => {
         if (text() == "") return;
         setError(false);
         setSending(true);
-        let result = await postClip(text());      
+        let result = await postClip(text(), reciever());      
         if (result) {
            refetchClips(); 
         }
@@ -26,7 +27,11 @@ export default function AddClipForm() {
         <div class="mt-16 mb-16 max-w-md flex flex-col">
             <h2>Send me a message</h2>
 
+            <div>Message</div> 
             <input class={inputClass} value={text()} onChange={event => setText(event.currentTarget.value)}/>
+
+            <div>Reciever (optional)</div>
+            <input class={inputClass} value={reciever()} onChange={event => setReciever(event.currentTarget.value)}/>
             
             <button class={buttonClass} onClick={sendAction}>Send</button>
            
